b603eeb6e16161ca07eb798f6b554da27ee70b74
Vadym Strelchenko <bowlingforsoap@mail.ru>
2019-05-25 21:41:31 +0200

    added bubble graphics

---
+++ Assets/Scripts/Graphics/BubbleGraphics.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Graphical representation of a Bubble.
+/// </summary>
+public class BubbleGraphics : MonoBehaviour
+{
+    public SpriteRenderer bubbleCenterRenderer;
+    public Text bubbleValueText;
+
+    public Bubble Bubble { get; set; }
+
+    public Color[] exponentColors;
+
+    private void Start()
+    {
+    }
+
+    public void SyncGraphicsWithBubble()
+    {
+        bubbleCenterRenderer.color = exponentColors[Bubble.Exponent];
+        bubbleValueText.text = Bubble.Value.ToString();
+    }
+}
